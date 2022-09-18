a4de44d5ef28e66eba53d1d0cc65259fd719b36c
willnotsin <wedafe45@gmail.com>
2022-09-18 14:48:08 +0100

    imported the necessaries: icons, axios and hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
+//import axios: for calling APIs
+import axios from "axios";
+
+//import icons
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner10 } from "react-icons/im";
 
 const App = () => {
   return <div>react app</div>;
